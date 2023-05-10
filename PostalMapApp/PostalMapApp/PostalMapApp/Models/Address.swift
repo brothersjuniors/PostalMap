a7489a55be2b6065a2453a6e34aa09fbbec8877b
brothersjuniors <101852740+brothersjuniors@users.noreply.github.com>
2023-05-11 07:43:58 +0900

    first commit

---
+++ PostalMapApp/PostalMapApp/PostalMapApp/Models/Address.swift
@@ -0,0 +1,21 @@
+//
+//  Address.swift
+//  PostalMapApp
+//
+//  Created by 近江伸一 on 2023/01/04.
+//
+
+
+import Foundation
+import CoreLocation
+
+    
+    struct Address {
+        var administrativeArea = "" // 都道府県
+        var locality = ""// 市区町村
+        var subLocality = "" // 地名
+        var thoroughfare = ""//　丁目
+        var subThoroughfare = ""//番地
+    }
+    
+    
